d44b3f699273c4e6678189613ca82b4a3dc91481
skexiacn4 <117562304+skexiacn4@users.noreply.github.com>
2022-11-06 23:49:09 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var objPeople = [
+    {
+        username: "royilao",
+        password: "061421"
+        
+    },
+]
+
+function getInfo() {
+    var username = document.getElementById("username").value
+    var password = document.getElementById("password").value
+}
